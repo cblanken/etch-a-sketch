770267328e2910abf1383f1441c9eaa2c4726aa6
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2021-09-23 23:51:00 -0400

    Initial setup for etch-a-sketch
    Includes grid generation and basic flexbox and css grid layouts with hover effect on grid.
    Resize form added. No functionality yet.

---
+++ etch.js
@@ -0,0 +1,28 @@
+// Etch-a-Sketch 
+const gridWidth = 32;
+const gridHeight = 32;
+
+const gridContainer = document.querySelector("#grid-container");
+
+function generateGrid(root, width, height) {
+    // Create 2d array for grid
+    let grid = Array.from(Array(width), () => new Array(height));
+    // Create elements for each grid cell
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            const cell = document.createElement("div");
+            cell.classList.add("square");
+            grid.push(cell);
+            root.appendChild(cell);
+        }
+    }
+    
+    return grid;
+}
+
+function updateGridCell(grid, x, y, text) {
+    grid[x][y].textContent = text; 
+}
+
+let grid = generateGrid(gridContainer, gridWidth, gridHeight)
+
