c9b1c11b4c15e8a51d4be8df507725e5ca80edf4
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2021-09-28 17:15:47 -0400

    Add resize grid functionality

--- etch.js
+++ etch.js
@@ -1,12 +1,14 @@
 // Etch-a-Sketch 
 
-
 // Grid setup
-const gridWidth = 32;
-const gridHeight = 32;
-
+const gridWidth = 8;
+const gridHeight = 8;
 const gridContainer = document.querySelector("#grid-container");
-let drawnOverCells = [] 
+
+// Generate default grid
+// NOTE: Cells can be referenced as grid[row][column]
+let grid = generateGrid(gridContainer, gridWidth, gridHeight)
+let drawnOverCells = [];
 
 function generateGrid(container, width, height) {
     // Create 2d array for grid
@@ -14,16 +16,40 @@ function generateGrid(container, width, height) {
     // Create elements for each grid cell
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-            const cell = document.createElement("div");
-            cell.classList.add("square");
-            grid.push(cell);
-
-            cell.addEventListener("mouseover", mouseover);
-            container.appendChild(cell);
+            addCell(container, grid, x, y);
         }
     }
     
     return grid;
+}
+
+function addCell(container, grid, x, y) {
+    const cell = document.createElement("div");
+    cell.classList.add("square");
+    grid[x][y] = cell;
+
+    cell.addEventListener("mouseover", mouseover);
+    container.appendChild(cell);
+
+    return grid;
+}
+
+function clearGrid() {
+    gridContainer.innerHTML = ""
+    grid.length = 0;
+}
+
+function removeCell(container, x, y) {
+    const offset = x * (y - 1) + y;
+    cell = document.querySelectorAll("#grid-container div")[offset];
+    cell.remove();
+}
+
+function updateSketchPad(container, grid) {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+        // TODO: update divs with grid param 
+    }
 }
 
 function mouseover(e) {
@@ -37,21 +63,70 @@ function updateGridCell(grid, x, y, text) {
     grid[x][y].textContent = text; 
 }
 
-function resetGridCell(cell) {
+function eraseGridCell(cell) {
     if (cell.classList.contains("drawn-over")) {
         cell.classList.remove("drawn-over");
     }
 }
 
-function resetGrid() {
-    drawnOverCells.forEach(cell => resetGridCell(cell)); 
+// UI setup
+function eraseGrid() {
+    drawnOverCells.forEach(cell => eraseGridCell(cell)); 
+    drawnOverCells.length = 0;
 }
 
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", eraseGrid);
 
-let grid = generateGrid(gridContainer, gridWidth, gridHeight)
+function addGridCol() {
+    for (let x = 0; x < grid.length; x++) {
+        grid[x].push();
+        //addCell(gridContainer, grid, grid.length - 1, y);
+        // TODO: link css width and height variables
+    }
+}
 
+function addGridRow() {
+    grid.push(new Array(grid[0].length));
+    for (let y = 0; y < grid[0].length; y++) {
+        addCell(gridContainer, grid, grid.length - 1, y);
+        // TODO: link css width and height variables
+    }
+}
 
-// UI setup
-const resetButton = document.querySelector("#reset-button");
-resetButton.addEventListener("click", resetGrid);
+function removeGridCol() {
+
+}
+
+function removeGridRow() {
+     
+}
+
+function resizeGrid(width, height) {
+    try {
+        const html = document.querySelector("html");
+        console.log("Resizing grid...");
+        const xDiff = width - gridWidth;
+        const yDiff = height - gridHeight;
+        console.log(`Diff: ${xDiff}, ${yDiff}`);
+        
+        clearGrid();
+        grid = generateGrid(gridContainer, width, height);
+        html.style.setProperty('--grid-width', width);
+        html.style.setProperty('--grid-height', height);
+    }
+    catch (e) {
+        alert(`${e}\nEnter valid dimensions before resizing.`); 
+    }
+}
+
+function handleResizeClick(e) {
+    e.preventDefault(); // default behavior includes reloading the page
+    const width = Number(document.querySelector("#width-input").value);
+    const height = Number(document.querySelector("#height-input").value);
+    resizeGrid(width, height);
+}
+
+const resizeButton = document.querySelector("#resize-button");
+resizeButton.addEventListener("click", handleResizeClick);
 
